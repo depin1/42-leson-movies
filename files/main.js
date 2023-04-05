f76bdce7aa2c5e054d11153900d8bcb13a3b9bbf
depin1 <boymatobvnusratillo1@gmail.com>
2023-04-05 20:19:57 +0500

    main

--- files/main.js
+++ files/main.js
@@ -33,7 +33,7 @@
          let response = await fetch(url);
          let data = await response.json();
 
-         let totalpages = data.total_pages > 5 ? 5 : data.total_pages
+         let totalpages = data.total_pages > 5 ? 5 : data.total_pages;
 
 
          movieList.innerHTML = '';
@@ -41,21 +41,22 @@
          //  let datares = data.results;
 
 
-
          if (data.results.length == 0) {
-             input.classList.add('active')
+
 
              let notfound = document.createElement('div');
              notfound.textContent = '404 Hech qanday malumot yoq';
              notfound.classList.add('title')
 
-
-             input.value = '';
              pageNAtionWrapper.appendChild(notfound);
 
+             input.classList.add('active')
+
          } else {
-             //  let dataslice = data.results.slice(0, 5)
-             data.results.map((movie) => {
+             input.classList.remove('active')
+
+             let dataslice = data.results.slice(0, 4)
+             dataslice.map((movie) => {
                  const image = document.createElement('img');
                  image.src = ` ${imagesUrl}${movie.backdrop_path}`;
                  image.alt = movie.title;;
@@ -67,6 +68,7 @@
                  let minbox = document.createElement('div');
                  minbox.classList.add('minbox')
 
+
                  fragment.appendChild(image);
                  fragment.appendChild(title);
                  minbox.appendChild(fragment);
@@ -77,8 +79,29 @@
 
              pageNAtionWrapper.innerHTML = '';
 
+             if (totalpages > 1) {
+
+                 for (let i = 1; i < totalpages; i++) {
+
+                     let button = document.createElement('button');
+
+                     button.innerText = i;
+                     pageNAtionWrapper.appendChild(button)
+                     if (curientpage === i) {
+                         button.classList.add('activebtn')
+                     } else {
+                         button.addEventListener('click', () => {
+                             curientpage = i;
+                             featchDat();
+
+                         })
+                     }
+                 }
+
+             }
+
          }
-         input.value = '';
+
      } catch (error) {
          console.log(error);
      }
@@ -95,116 +118,3 @@
  btn.addEventListener('click', () => {
      featchDat();
  })
-
- //Teacher cods
-
-
-
- //  const apiKey = 'c1ba5d7054ad4225561ffacb783be3c6';
- //  const baseUrl = 'https://api.themoviedb.org/3/';
-
- //  const imagesUrl = 'https://image.tmdb.org/t/p/w500';
-
-
- //  const fragment = document.createDocumentFragment();
-
-
- //  const loader_container = document.querySelector('.loader-container');
- //  const loader = document.createElement('div');
- //  loader.classList.add('loader');
-
- //  loader_container.appendChild(loader);
-
- //  let currentPage = 1;
-
- //  async function getMovies() {
- //      const searchTerm = document.querySelector('#search').value;
-
- //      let url;
-
- //      if (searchTerm) {
- //          url = `${baseUrl}search/movie?api_key=${apiKey}&query=${searchTerm}`;
- //      } else {
- //          url = `${baseUrl}discover/movie?api_key=${apiKey}&page=${currentPage}`;
- //      }
-
- //      try {
- //          const response = await fetch(url);
- //          const data = await response.json();
-
- //          let totalPages = data.total_pages > 5 ? 5 : data.total_pages;
-
- //          const movie_container = document.querySelector('.movie-container');
- //          movie_container.innerHTML = '';
- //          if (data.results.length === 0) {
- //              const message = document.querySelector('.not-found');
- //              message.textContent = 'Siz izlagan kino topilmadi'
- //          } else {
- //              data.results.map((movie) => {
- //                  const movieCard = document.createElement('div');
- //                  movieCard.classList.add('movie-card');
-
- //                  const movieImage = document.createElement('img');
- //                  movieImage.src = `${imagesUrl}${movie.backdrop_path}`;
- //                  movieImage.alt = movie.title;
-
- //                  const movieTitle = document.createElement('h3');
- //                  movieTitle.textContent = movie.title;
-
- //                  const movieRelease = document.createElement('span');
- //                  movieRelease.textContent = `Released: ${movie.release_date}`;
-
-
- //                  fragment.appendChild(movieImage);
- //                  fragment.appendChild(movieTitle);
- //                  fragment.appendChild(movieRelease);
- //                  movieCard.appendChild(fragment);
- //                  movie_container.appendChild(movieCard);
- //              })
-
- //              const paginationWrapper = document.querySelector('.pagination-wrapper');
- //              paginationWrapper.innerHTML = '';
-
- //              if (totalPages > 1) {
-
- //                  for (let i = 1; i <= totalPages; i++) {
- //                      const button = document.createElement('button');
- //                      button.innerText = i;
- //                      if (currentPage === i) {
- //                          button.classList.add('active');
- //                      }
- //                      button.addEventListener('click', () => {
- //                          currentPage = i;
- //                          getMovies();
- //                      })
-
- //                      paginationWrapper.appendChild(button);
- //                  }
- //              }
- //          }
-
- //      } catch (error) {
- //          console.error(error.message);
- //      } finally {
- //          const loader = document.querySelector('.loader');
- //          if (loader) {
- //              loader_container.remove();
- //          }
- //      }
- //  }
-
- //  const searchInput = document.querySelector('#search');
- //  const btn = document.querySelector('#searchBtn');
-
- //  searchInput.addEventListener('keypress', (e) => {
- //      if (e.key === 'Enter') {
- //          getMovies();
- //      }
- //  });
- //  btn.addEventListener('click', () => {
-
- //      currentPage = 1;
- //      getMovies();
- //  });
-
- //  getMovies();
