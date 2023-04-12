8116fb79e48a406b8909331eac318431e3ed3d00
depin1 <boymatobvnusratillo1@gmail.com>
2023-04-12 07:42:16 +0500

    main

--- files/main.js
+++ files/main.js
@@ -6,20 +6,28 @@
  const section = document.querySelector('.container');
  const loader = document.createElement('div');
 
- //  loader.classList.add('loading')
-
 
  const imagesUrl = 'https://image.tmdb.org/t/p/w500';
  let url = 'https://api.themoviedb.org/3/movie/550?api_key=23adc2ee67096b17043b119fd5813b57';
  const searchUrl = 'https://api.themoviedb.org/3/search/movie?api_key=e5d48ddea98c83cfcc9a7df314611468&query=';
 
+ //  async function datafatch() {
 
+ //      try {
+ //          const response = await fetch(url);
+ //          const data = await response.json();
+ //          console.log(data);
 
- //  let row = document.querySelector('.row');
+ //      } catch (error) {
+ //          console.log(error);
+ //      }
+ //  }
+ //  datafatch();
+
  let pageNAtionWrapper = document.querySelector('.pagenation');
  const fragment = document.createDocumentFragment();
 
- let curientpage = 1;
+ let curientpage = 15;
 
  async function featchDat() {
      const inputCondition = input.value;
@@ -32,13 +40,12 @@
 
          let response = await fetch(url);
          let data = await response.json();
-
-         let totalpages = data.total_pages > 5 ? 5 : data.total_pages;
+         console.log(data.results);
+         let totalpages = data.total_pages > 19 ? 25 : data.total_pages;
 
 
          movieList.innerHTML = '';
 
-         //  let datares = data.results;
 
 
          if (data.results.length == 0) {
@@ -54,8 +61,9 @@
 
          } else {
              input.classList.remove('active')
-
-             let dataslice = data.results.slice(0, 4)
+             console.log(data);
+             //.slice(0, 8)
+             let dataslice = data.results;
              dataslice.map((movie) => {
                  const image = document.createElement('img');
                  image.src = ` ${imagesUrl}${movie.backdrop_path}`;
@@ -79,9 +87,9 @@
 
              pageNAtionWrapper.innerHTML = '';
 
-             if (totalpages > 1) {
+             if (totalpages > 20) {
 
-                 for (let i = 1; i < totalpages; i++) {
+                 for (let i = 15; i < totalpages; i++) {
 
                      let button = document.createElement('button');
 
